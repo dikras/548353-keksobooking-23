b1011d468161fcce83a84826cc6184da470de848
Dmitry Krasyukov <dmi1517@yandex.ru>
2021-06-07 21:09:47 +0300

    Скорректировал нейминг

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@ const TITLES = [
   'Многозвездочный отель с круглосуточной спа-зоной',
   'Деревянный домик с расписными ставнями',
   'Двухэтажное бунгало на сваях и с прозрачным полом',
-  'Дворец в стиле венецианско барокко',
+  'Дворец в стиле венецианское барокко',
 ];
 
-const ACCOMODATION_TYPES = [
+const TYPES = [
   'palace',
   'flat',
   'house',
@@ -56,81 +56,91 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const MIN_PRICE = 1000;
-const MAX_PRICE = 300000;
+const Price = {
+  MIN: 1000,
+  MAX: 300000,
+};
 
-const MIN_ROOMS = 2;
-const MAX_ROOMS = 20;
+const Room = {
+  MIN: 2,
+  MAX: 20,
+};
 
-const MIN_GUESTS = 1;
-const MAX_GUESTS = 40;
+const Guest = {
+  MIN: 1,
+  MAX: 40,
+};
 
-const MIN_LATITUDE = 35.65000;
-const MAX_LATITUDE = 35.70000;
+const Latitude = {
+  MIN: 35.65000,
+  MAX: 35.70000,
+};
 
-const MIN_LONGITUDE = 139.70000;
-const MAX_LONGITUDE = 139.80000;
+const Longitude = {
+  MIN: 139.70000,
+  MAX: 139.80000,
+};
 
-const DIGITS_AFTER_POINT = 5;
+const DIGITS = 5;
 
 const SIMILAR_OFFERS_COUNT = 10;
 
 // Вспомогательные функции
-function getRandomPositiveInteger (firstNum, secondNum) {
-  const lower = Math.ceil(Math.min(Math.abs(firstNum), Math.abs(secondNum)));
-  const upper = Math.floor(Math.max(Math.abs(firstNum), Math.abs(secondNum)));
+const getRandomInteger = (firstNumber, secondNumber) => {
+  const lower = Math.ceil(Math.min(Math.abs(firstNumber), Math.abs(secondNumber)));
+  const upper = Math.floor(Math.max(Math.abs(firstNumber), Math.abs(secondNumber)));
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
-function getRandomPositiveFloat (firstNum, secondNum, digits = 1) {
-  const lower = Math.min(Math.abs(firstNum), Math.abs(secondNum));
-  const upper = Math.max(Math.abs(firstNum), Math.abs(secondNum));
+const getRandomFloat = (firstNumber, secondNumber, digits = 1) => {
+  const lower = Math.min(Math.abs(firstNumber), Math.abs(secondNumber));
+  const upper = Math.max(Math.abs(firstNumber), Math.abs(secondNumber));
   const result = Math.random() * (upper - lower) + lower;
 
   return result.toFixed(digits);
-}
+};
 
-const getRandomArrayElement = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+const getRandomItem = (items) => items[getRandomInteger(0, items.length - 1)];
 
-const getRandomArray = (array) => {
-  const randomIndex = getRandomPositiveInteger(0, array.length - 1);
-  const randomArray = array.slice(0, randomIndex + 1);
-  return randomArray;
+const getRandomFeatures = (items) => {
+  const randomIndex = getRandomInteger(0, items.length - 1);
+  const randomFeatures = items.slice(0, randomIndex + 1);
+  return randomFeatures;
 };
 
 const getStringLocation = () => {
-  const latitude = getRandomPositiveFloat(MIN_LATITUDE, MAX_LATITUDE, DIGITS_AFTER_POINT);
-  const longitude = getRandomPositiveFloat(MIN_LONGITUDE, MAX_LONGITUDE, DIGITS_AFTER_POINT);
+  const latitude = getRandomFloat(Latitude.MIN, Latitude.MAX, DIGITS);
+  const longitude = getRandomFloat(Longitude.MIN, Longitude.MAX, DIGITS);
 
   return `${latitude}, ${longitude}`;
 
 };
 
 // Создание массива похожих объявлений
-function createOffer() {
-  return {
-    author: getRandomArrayElement(AVATARS),
+const createOffer = () => (
+  {
+    author: getRandomItem(AVATARS),
     offer: {
-      title: getRandomArrayElement(TITLES),
+      title: getRandomItem(TITLES),
       address: getStringLocation(),
-      price: getRandomPositiveInteger(MIN_PRICE, MAX_PRICE),
-      type: getRandomArrayElement(ACCOMODATION_TYPES),
-      rooms: getRandomPositiveInteger(MIN_ROOMS, MAX_ROOMS),
-      guests: getRandomPositiveInteger(MIN_GUESTS, MAX_GUESTS),
-      checkin: getRandomArrayElement(CHECKIN_TIMES),
-      checkout: getRandomArrayElement(CHECKOUT_TIMES),
-      features: getRandomArray(FEATURES),
+      price: getRandomInteger(Price.MIN, Price.MAX),
+      type: getRandomItem(TYPES),
+      rooms: getRandomInteger(Room.MIN, Room.MAX),
+      guests: getRandomInteger(Guest.MIN, Guest.MAX),
+      checkin: getRandomItem(CHECKIN_TIMES),
+      checkout: getRandomItem(CHECKOUT_TIMES),
+      features: getRandomFeatures(FEATURES),
       description: 'Удобное расположение, оригинальный дизайн комнат, прекрасные виды из окон.',
-      photos: getRandomArray(PHOTOS),
+      photos: getRandomFeatures(PHOTOS),
     },
     location: {
-      lat: getRandomPositiveFloat(MIN_LATITUDE, MAX_LATITUDE, DIGITS_AFTER_POINT),
-      lng: getRandomPositiveFloat(MIN_LONGITUDE, MAX_LONGITUDE, DIGITS_AFTER_POINT),
+      lat: getRandomFloat(Latitude.MIN, Latitude.MAX, DIGITS),
+      lng: getRandomFloat(Longitude.MIN, Longitude.MAX, DIGITS),
     },
-  };
-}
+  }
+);
 
 const similarOffers = new Array(SIMILAR_OFFERS_COUNT).fill(null).map(() => createOffer());
 
