239d16ba7c74f6ce205aca58ac6e9b4c26087ebb
Dmitry Krasyukov <dmi1517@yandex.ru>
2021-07-13 12:12:30 +0300

    Перламутровые пуговицы

--- js/filters.js
+++ js/filters.js
@@ -1,44 +1,56 @@
 import { getData } from './api.js';
 import { mapFilters } from './map.js';
 import { renderPins, markerGroup } from './similar-offers.js';
-import { isInRange } from './util.js';
 
 const SIMILAR_OFFERS_COUNT = 10;
+const ANY_VALUE = 'any';
+const RADIX = 10;
 
-const PriceRange = {
+const HousingPriceRange = {
   LOW: {
     MIN: 0,
-    MAX: 9999
+    MAX: 10000,
   },
   MIDDLE: {
-    MIN: 10000,
-    MAX: 49999
+    MIN: 10001,
+    MAX: 50000,
   },
   HIGH: {
-    MIN: 50000,
-    MAX: 1000000
-  }
+    MIN: 50001,
+    MAX: 1000000,
+  },
 };
 
-// DOM-элементы для фильтров
-const mainFilter = document.querySelector('.map__filters');
 const filterHousingType = mapFilters.querySelector('#housing-type');
 const filterHousingPrice = mapFilters.querySelector('#housing-price');
 const filterRoomsNumber = mapFilters.querySelector('#housing-rooms');
 const filterGuestsNumber = mapFilters.querySelector('#housing-guests');
-// const filterHousingFeatures = mapFilters.querySelector('#housing-features');
+const filterHousingFeatures = mapFilters.querySelector('#housing-features');
 // const housingFeatureElements = filterHousingFeatures.querySelectorAll('input[name="features"]');
 
-const filterItem = (it, item, key) => it.value === 'any' ? true : it.value === item[key].toString();
+// Ф-ии фильтрации
+const filterByHousingType = (ad) => filterHousingType.value === ANY_VALUE ? true : ad.offer.type === filterHousingType.value;
 
-const filterByType = (item) => filterItem(filterHousingType, item.offer, 'type');
-const filterByPrice = (item) => {
-  const filteringPrice = PriceRange[filterHousingPrice.value.toUpperCase()];
-  return filteringPrice ? item.offer.price >= filteringPrice.MIN && item.offer.price <= filteringPrice.MAX : true;
+const filterByHousingPrice = (ad) => {
+  const filteringPriceRange = HousingPriceRange[filterHousingPrice.value.toUpperCase()];
+  return filteringPriceRange ? ad.offer.price >= filteringPriceRange.MIN && ad.offer.price <= filteringPriceRange.MAX : true;
 };
 
-const onMainFilterChange = () => {
+const filterByRoomsNumber = (ad) => filterRoomsNumber.value === ANY_VALUE ? true : ad.offer.rooms === parseInt(filterRoomsNumber.value, RADIX);
 
+const filterByGuestsNumber = (ad) => filterGuestsNumber.value === ANY_VALUE ? true : ad.offer.guests === parseInt(filterGuestsNumber.value, RADIX);
+
+const filterByFeatures = (ad) => {
+  const checkedFeaturesItems = filterHousingFeatures.querySelectorAll('input:checked');
+  return Array.from(checkedFeaturesItems).every((element) => ad.offer.features.includes(element.value));
 };
+
+mapFilters.addEventListener('change', () => {
+  getData((offers) => {
+    const similarOffers = offers.filter(filterByHousingType).filter(filterByHousingPrice).filter(filterByRoomsNumber).filter(filterByGuestsNumber).filter(filterByFeatures);
+    markerGroup.clearLayers();
+    renderPins(similarOffers.slice(0, SIMILAR_OFFERS_COUNT));
+  });
+});
 
 export { SIMILAR_OFFERS_COUNT };
